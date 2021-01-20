6c6a53e89ac6407dc640bb9cb586c93cd6003ffe
jaya <jayaseyyadri@gmail.com>
2021-01-20 16:13:16 -0600

    completed servlets counter

--- src/main/java/PageViewCounter.java
+++ src/main/java/PageViewCounter.java
@@ -1,21 +1,20 @@
-//import javax.servlet.annotation.WebServlet;
-//import javax.servlet.http.HttpServlet;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import java.io.IOException;
-//import java.io.PrintWriter;
-//
-//    @WebServlet(name="PageViewCounter",urlPatterns ="/counter")
-//    public class PageViewCounter extends HttpServlet {
-//        @Override
-//        protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
-//            res.setContentType("text/html");
-//            PrintWriter out = res.getWriter();
-//
-//            int count;
-//
-//            out.println("<h1>  This page has been viewed " + </h1>");
-//        }
-//
-//    }
-//}
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+    @WebServlet(name="PageViewCounter",urlPatterns ="/counter")
+    public class PageViewCounter extends HttpServlet {
+
+        int counter=1;
+        @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+            res.setContentType("text/html");
+            PrintWriter out = res.getWriter();
+            counter+=1;
+            out.println(" <h1>  This page has been viewed  </h1>" +"<h1>" +counter +"</h1>");
+        }
+
+    }
