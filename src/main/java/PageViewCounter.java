067733a295f9a82b30014b7a9770d02416870109
jaya <jayaseyyadri@gmail.com>
2021-01-20 10:53:15 -0600

    working on Page View Counter

---
+++ src/main/java/PageViewCounter.java
@@ -0,0 +1,21 @@
+//import javax.servlet.annotation.WebServlet;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//import java.io.PrintWriter;
+//
+//    @WebServlet(name="PageViewCounter",urlPatterns ="/counter")
+//    public class PageViewCounter extends HttpServlet {
+//        @Override
+//        protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+//            res.setContentType("text/html");
+//            PrintWriter out = res.getWriter();
+//
+//            int count;
+//
+//            out.println("<h1>  This page has been viewed " + </h1>");
+//        }
+//
+//    }
+//}
